57a0b394e7463c96a8aa32298c2504d4a2917af3
Petros Simonyan <petros20003@gmail.com>
2023-09-19 15:25:18 +0200

    UI added

--- DemoApp/UserInterface.cs
+++ DemoApp/UserInterface.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DemoApp
 {
-    public partial class Form2 : Form
+    public partial class UserInterface : Form
     {
-        public Form2()
+        public UserInterface()
         {
             InitializeComponent();
         }
