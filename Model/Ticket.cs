f3e3a76f42c3438f5f24d70ddc401a86af22ddcb
pierickm <113185406+pierickm@users.noreply.github.com>
2023-09-19 14:41:14 +0200

    change connection string add enums create TicketModel

---
+++ Model/Ticket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Model
+{
+    internal class Ticket
+    {
+        [BsonElement("id")]
+        public string Id { get; set; }
+
+        [BsonElement("date_reported")]
+        public DateTime DateReported { get; set; }
+
+        [BsonElement("subject")]
+        public string subject { get; set; }
+
+        [BsonElement("incident_type")]
+        public string incidentType { get; set; } //change to enumeration
+
+        /*
+        [BsonElement("reported_by")]
+        public User reportedBy {get; set;}
+        */
+
+        [BsonElement("priority")]
+        public string priority { get; set; } // change to enum
+
+        [BsonElement("deadline")]
+        public DateTime deadline { get; set; }
+
+        [BsonElement("description")]
+        public string description { get; set; }
+
+        [BsonElement("status")]
+        public string status { get; set; } //change to enum
+
+    }
+}
