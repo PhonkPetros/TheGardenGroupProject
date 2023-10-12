3c18885613b99461667d6dd98df55445fe8b5c59
pierickm <113185406+pierickm@users.noreply.github.com>
2023-10-12 10:20:10 +0200

    make changeticket view

--- DemoApp/ChangeTicketView.cs
+++ DemoApp/ChangeTicketView.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace DemoApp
 {
     public class ChangeTicketView
     {
+        private Ticket selectedTicket;
 
+        public ChangeTicketView(Ticket selectedTicket)
+        {
+
+        }
     }
 }
